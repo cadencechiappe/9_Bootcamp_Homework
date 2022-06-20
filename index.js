3add44837a4b52fea9643e656c51d2e8c14c9438
Cadence Chiappe <cadence@chiappe.com>
2022-06-20 15:03:52 -0500

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'Project Title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Description of Project',
+  },
+  {
+    type: 'input',
+    name: 'instructions',
+    message: 'Specific Installation Instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage information?',
+  },
+  {
+    type: 'input',
+    name: 'guidelines',
+    message: 'List contribution guidelines',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What are the test instructions?',
+  },];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
